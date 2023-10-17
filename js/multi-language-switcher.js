afcb2916ed0a6d201b0cd614ba761e6b9430aa3e
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-17 09:46:05 +0200

    remember last language selection

--- js/multi-language-switcher.js
+++ js/multi-language-switcher.js
@@ -1,3 +1,21 @@
+function setLastLanguage(lang)
+{
+    lang = lang.toLowerCase();
+    if(typeof(Storage) !== 'undefined')
+        localStorage.setItem('lastSelectedLanguage', lang);
+}
+
+function getLastLanguage()
+{
+    if(typeof(Storage) !== 'undefined')
+    {
+        const lang = localStorage.getItem('lastSelectedLanguage');
+        if(lang)
+            return lang;
+    }
+    return 'python';
+}
+
 document.querySelectorAll('.multi-language-switcher').forEach(multiLanguageSwitcher => {
     const tabsContent = document.createElement('div');
     tabsContent.className = 'tabs-content';
@@ -15,9 +33,25 @@ document.querySelectorAll('.multi-language-switcher').forEach(multiLanguageSwitc
 
 
     const codeElements = tabsContent.querySelectorAll('code.hljs');
-    var first = true;
+
+    // collect langs:
+    var languages = [];
     codeElements.forEach(function(codeElement) {
-        if(!first) codeElement.style.display = 'none';
+        const classNames = codeElement.className.split(' ');
+        classNames.forEach(function(className) {
+            if(className.startsWith('language-')) {
+                var languageName = className.replace(/^language-/, '');
+                languages.push(languageName);
+            }
+        });
+    });
+    console.log('Languages', languages);
+
+    var lastLangIdx = Math.max(0, languages.indexOf(getLastLanguage()));
+
+    var index = 0;
+    codeElements.forEach(function(codeElement) {
+        if(index != lastLangIdx) codeElement.style.display = 'none';
 
         const classNames = codeElement.className.split(' ');
         classNames.forEach(function(className) {
@@ -28,8 +62,9 @@ document.querySelectorAll('.multi-language-switcher').forEach(multiLanguageSwitc
                 const button = document.createElement('button');
                 button.innerText = languageName;
                 button.setAttribute('data-id', className);
-                if(first) button.classList.add('active');
+                if(index == lastLangIdx) button.classList.add('active');
                 button.addEventListener('click', e => {
+                    setLastLanguage(languageName);
                     tabsHeader.querySelectorAll('button').forEach(e => e.classList.remove('active'));
                     button.classList.add('active');
                     tabsContent.querySelectorAll('code.hljs').forEach(e => e.style.display = 'none');
@@ -39,6 +74,6 @@ document.querySelectorAll('.multi-language-switcher').forEach(multiLanguageSwitc
             }
         });
 
-        first = false;
+        index++;
     });
 });
