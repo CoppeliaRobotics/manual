ca560b4e8570c67d2dbd2710b4ec267679b2da1f
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-18 15:25:19 +0200

    XML

--- genRegularApi.py
+++ genRegularApi.py
@@ -2,11 +2,14 @@ import os
 import sys
 import re
 import shutil
+import xml.etree.ElementTree as ET
 
 from pathlib import Path
 
 
 currentDir = Path(__file__).absolute().parent
+
+templatesDir = currentDir / 'templates'
 
 zmqRemoteApiToolsDir = currentDir.parent / 'programming' / 'zmqRemoteApi' / 'tools'
 if zmqRemoteApiToolsDir.is_dir():
@@ -20,135 +23,127 @@ from calltip import FuncDef, Arg, ArgDef
 
 
 def main():
-    inputFile = currentDir / 'regularApi.txt'
-    regApiDir = currentDir / 'en' / 'regularApi'
+    regularApiDir = currentDir / 'en' / 'regularApi'
     try:
-        shutil.rmtree(regApiDir)
+        shutil.rmtree(regularApiDir)
     except Exception as e:
         pass
-    os.makedirs(regApiDir)
+    os.makedirs(regularApiDir)
 
-    temp = currentDir / 'templates' / 'regularApi_cpp.htm'
-    with temp.open('r') as file_r:
+    with (templatesDir / 'regularApi_cpp.htm').open('r') as file_r:
         template_cpp = file_r.read()
-    temp = currentDir / 'templates' / 'regularApi_pythonLua.htm'
-    with temp.open('r') as file_r:
-        template = file_r.read()
-    with inputFile.open('r') as file_r:
-        content = file_r.read()
-        pos = 0
-        allCpp = {}
-        allPythonLua = {}
-        unparseableCalltips = []
-        while pos != None:
-            apiFunc, pos = getTxt(content, 'apiFunctionName', pos)
-            _, posEnd = getTxt(content, 'apiFunctionName', pos)
-            if apiFunc:
-                apiSynopsisCpp, *_ = getTxt(content, 'apiSynopsisCpp', pos, posEnd)
-                apiSynopsisPython, *_ = getTxt(content, 'apiSynopsisPython', pos, posEnd)
-                apiSynopsisLua, *_ = getTxt(content, 'apiSynopsisLua', pos, posEnd)
-                if apiSynopsisCpp:
-                    allCpp[getCppName(apiFunc)] = True
-                if apiSynopsisPython or apiSynopsisLua:
-                    allPythonLua[getPythonLuaName(apiFunc)] = True
-        pos = 0
-        cnt = 0
-        while pos != None:
-            apiFunc, pos = getTxt(content, 'apiFunctionName', pos)
-            _, posEnd = getTxt(content, 'apiFunctionName', pos)
-            if apiFunc:
-                cnt += 1
-                apiDescription, *_ = getTxt(content, 'apiDescription', pos, posEnd)
-                apiSeeAlso, *_ = getTxt(content, 'apiSeeAlso', pos, posEnd)
-                if apiSeeAlso != None:
-                    apiSeeAlsoCpp = prepSeeAlso(apiSeeAlso, allCpp, allPythonLua, 'cpp')
-                    apiSeeAlsoPythonLua = prepSeeAlso(apiSeeAlso, allCpp, allPythonLua, 'pythonLua')
-                else:
-                    apiSeeAlsoCpp = ''
-                    apiSeeAlsoPythonLua = ''
-                apiSynopsisCpp, *_ = getTxt(content, 'apiSynopsisCpp', pos, posEnd)
-                apiInputCpp, *_ = getTxt(content, 'apiInputCpp', pos, posEnd)
-                apiOutputCpp, *_ = getTxt(content, 'apiOutputCpp', pos, posEnd)
-                apiMoreCpp, *_ = getTxt(content, 'apiMoreCpp', pos, posEnd)
-                if apiMoreCpp == None:
-                    apiMoreCpp = ''
-                apiSynopsisPython, *_ = getTxt(content, 'apiSynopsisPython', pos, posEnd)
-                apiSynopsisLua, *_ = getTxt(content, 'apiSynopsisLua', pos, posEnd)
-                apiInputPythonLua, *_ = getTxt(content, 'apiInputPythonLua', pos, posEnd)
-                apiOutputPythonLua, *_ = getTxt(content, 'apiOutputPythonLua', pos, posEnd)
-                apiMorePythonLua, *_ = getTxt(content, 'apiMorePythonLua', pos, posEnd)
-                if apiMorePythonLua == None:
-                    apiMorePythonLua = ''
-                hasPythonLuaVersion = False
-                if apiSynopsisLua:
-                    try:
-                        FuncDef.from_calltip(apiSynopsisLua)
-                    except lark.exceptions.LarkError as e:
-                        unparseableCalltips.append((apiSynopsisLua, str(e)))
-                if apiSynopsisPython or apiSynopsisLua:
-                    hasPythonLuaVersion = True
-                    nm = currentDir / 'en' / 'regularApi' / (getCppName(apiFunc) + '.htm')
-                    with nm.open('w') as file_w:
-                        a = template
-                        a = a.replace('__funcName__', apiFunc)
-                        a = a.replace('__funcDescription__', apiDescription)
-                        a = a.replace('__seeAlso__', apiSeeAlsoPythonLua)
-                        a = a.replace('__pythonSynopsis__', addCodeSection(formatSynopsis(apiSynopsisPython, 100), 'python'))
-                        a = a.replace('__luaSynopsis__', addCodeSection(formatSynopsis(apiSynopsisLua, 100), 'lua'))
-                        if apiInputPythonLua:
-                            a = a.replace('__input__', apiInputPythonLua)
-                            a = a.replace('__inputVisibility__', '')
-                        else:
-                            a = a.replace('__inputVisibility__', 'style="display: none;"')
-                        if apiOutputPythonLua:
-                            a = a.replace('__output__', apiOutputPythonLua)
-                            a = a.replace('__outputVisibility__', '')
-                        else:
-                            a = a.replace('__outputVisibility__', 'style="display: none;"')
-                        a = a.replace('__more__', apiMorePythonLua)
-                        file_w.write(a)
-                if apiSynopsisCpp:
-                    nm = currentDir / 'en' / 'regularApi' / (getCppName(apiFunc) + '_cpp' + '.htm')
-                    with nm.open('w') as file_w:
-                        a = template_cpp
-                        a = a.replace('__funcName__', getCppName(apiFunc))
-                        a = a.replace('__funcDescription__', apiDescription)
-                        a = a.replace('__seeAlso__', apiSeeAlsoCpp)
-                        a = a.replace('__cppSynopsis__', formatSynopsis(apiSynopsisCpp, 100))
-                        if apiInputCpp:
-                            a = a.replace('__input__', apiInputCpp)
-                            a = a.replace('__inputVisibility__', '')
-                        else:
-                            a = a.replace('__inputVisibility__', 'style="display: none;"')
-                        if apiOutputCpp:
-                            a = a.replace('__output__', apiOutputCpp)
-                            a = a.replace('__outputVisibility__', '')
-                        else:
-                            a = a.replace('__outputVisibility__', 'style="display: none;"')
-                        a = a.replace('__more__', apiMoreCpp)
-                        file_w.write(a)
-        print(f'\nTotal generated: {cnt}')
-        if unparseableCalltips:
-            print(f'{len(unparseableCalltips)} calltips could not be parsed', end='')
-            opt = '--show-parse-errors'
-            if opt in sys.argv:
-                print('\n')
-                for i, (calltip, error) in enumerate(unparseableCalltips):
-                    print(f'{i:-3d}) Calltip: {calltip}')
-                    print(f'     Error: {error}')
-            else:
-                print(f' (pass {opt} to view details)')
 
-def getTxt(string, item, spos, posEnd = None):
-    itemName_s = '==================== ' + item + ' ====================<'
-    p1 = string.find(itemName_s, spos)
-    if p1 != -1:
-        if posEnd == None or p1 < posEnd:
-            p1 += len(itemName_s)
-            itemName_e = '==================== ' + item + ' ====================>'
-            p2 = string.find(itemName_e, p1)
-            return string[p1:p2].strip(), p2
-    return None, None
+    with (templatesDir / 'regularApi_pythonLua.htm').open('r') as file_r:
+        template = file_r.read()
+
+    tree = ET.parse(currentDir / 'regularApi.xml')
+    funcs = tree.getroot()
+
+    allCpp = {}
+    allPythonLua = {}
+    unparseableCalltips = []
+
+    def getTxt(n):
+        node = func.find(n)
+        if node is not None:
+            # preserve HTML tags (returning node.text woudln't work):
+            txt = ET.tostring(node, 'unicode')
+            txt = txt.strip()
+            txt = re.sub(f'^<{n}>(.*)</{n}>$', r'\1', txt, flags=re.DOTALL)
+            return txt
+
+    for func in funcs:
+        if apiFunc := getTxt('api-function-name'):
+            if getTxt('api-synopsis-cpp'):
+                allCpp[getCppName(apiFunc)] = True
+            if getTxt('api-synopsis-python') or getTxt('api-synopsis-lua'):
+                allPythonLua[getPythonLuaName(apiFunc)] = True
+
+    cnt = 0
+    for func in funcs:
+        apiFunc = getTxt('api-function-name').strip()
+        assert apiFunc, f'missing <api-function-name>: {ET.tostring(func, "unicode")}'
+
+        cnt += 1
+        apiDescription = getTxt('api-description')
+        if apiSeeAlso := getTxt('api-see-also'):
+            apiSeeAlsoCpp = prepSeeAlso(apiSeeAlso, allCpp, allPythonLua, 'cpp')
+            apiSeeAlsoPythonLua = prepSeeAlso(apiSeeAlso, allCpp, allPythonLua, 'pythonLua')
+        else:
+            apiSeeAlsoCpp = ''
+            apiSeeAlsoPythonLua = ''
+        apiSynopsisCpp = getTxt('api-synopsis-cpp')
+        apiInputCpp = getTxt('api-input-cpp')
+        apiOutputCpp = getTxt('api-output-cpp')
+        apiMoreCpp = getTxt('api-more-cpp') or ''
+        apiSynopsisPython = getTxt('api-synopsis-python')
+        apiSynopsisLua = getTxt('api-synopsis-lua')
+        apiInputPythonLua = getTxt('api-input-python-lua')
+        apiOutputPythonLua = getTxt('api-output-python-lua')
+        apiMorePythonLua = getTxt('api-more-python-lua') or ''
+
+        if apiSynopsisLua:
+            try:
+                FuncDef.from_calltip(apiSynopsisLua)
+            except lark.exceptions.LarkError as e:
+                unparseableCalltips.append((apiSynopsisLua, str(e)))
+
+        assert (apiSynopsisPython or apiSynopsisLua or apiSynopsisCpp), f'missing <api-synopsys-*>: {ET.tostring(func)}'
+
+        if apiSynopsisPython or apiSynopsisLua:
+            nm = currentDir / 'en' / 'regularApi' / (getCppName(apiFunc) + '.htm')
+            with nm.open('w') as file_w:
+                a = template
+                a = a.replace('__funcName__', apiFunc)
+                a = a.replace('__funcDescription__', apiDescription)
+                a = a.replace('__seeAlso__', apiSeeAlsoPythonLua)
+                a = a.replace('__pythonSynopsis__', addCodeSection(formatSynopsis(apiSynopsisPython, 100), 'python'))
+                a = a.replace('__luaSynopsis__', addCodeSection(formatSynopsis(apiSynopsisLua, 100), 'lua'))
+                if apiInputPythonLua:
+                    a = a.replace('__input__', apiInputPythonLua)
+                    a = a.replace('__inputVisibility__', '')
+                else:
+                    a = a.replace('__inputVisibility__', 'style="display: none;"')
+                if apiOutputPythonLua:
+                    a = a.replace('__output__', apiOutputPythonLua)
+                    a = a.replace('__outputVisibility__', '')
+                else:
+                    a = a.replace('__outputVisibility__', 'style="display: none;"')
+                a = a.replace('__more__', apiMorePythonLua)
+                file_w.write(a)
+        if apiSynopsisCpp:
+            nm = currentDir / 'en' / 'regularApi' / (getCppName(apiFunc) + '_cpp' + '.htm')
+            with nm.open('w') as file_w:
+                a = template_cpp
+                a = a.replace('__funcName__', getCppName(apiFunc))
+                a = a.replace('__funcDescription__', apiDescription)
+                a = a.replace('__seeAlso__', apiSeeAlsoCpp)
+                a = a.replace('__cppSynopsis__', formatSynopsis(apiSynopsisCpp, 100))
+                if apiInputCpp:
+                    a = a.replace('__input__', apiInputCpp)
+                    a = a.replace('__inputVisibility__', '')
+                else:
+                    a = a.replace('__inputVisibility__', 'style="display: none;"')
+                if apiOutputCpp:
+                    a = a.replace('__output__', apiOutputCpp)
+                    a = a.replace('__outputVisibility__', '')
+                else:
+                    a = a.replace('__outputVisibility__', 'style="display: none;"')
+                a = a.replace('__more__', apiMoreCpp)
+                file_w.write(a)
+
+    print(f'\nTotal generated: {cnt}')
+
+    if unparseableCalltips:
+        print(f'{len(unparseableCalltips)} calltips could not be parsed', end='')
+        opt = '--show-parse-errors'
+        if opt in sys.argv:
+            print('\n')
+            for i, (calltip, error) in enumerate(unparseableCalltips):
+                print(f'{i:-3d}) Calltip: {calltip}')
+                print(f'     Error: {error}')
+        else:
+            print(f' (pass {opt} to view details)')
 
 def addCodeSection(string, lang):
     s = ''
