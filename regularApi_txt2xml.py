3d2e21b0b91690856b7dd78040e3bdd5a33cc054
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-18 11:23:11 +0200

    txt2xml tool

---
+++ regularApi_txt2xml.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+currentDir = Path(__file__).absolute().parent
+
+records = []
+record = {}
+field_content = ''
+field_name = None
+line_num = 0
+with (currentDir / 'regularApi.txt').open('r') as f:
+    for line in f:
+        line_num += 1
+        line_trim = line.rstrip('\r\n')
+        words = line_trim.split() + [''] * 3
+        if line_trim == '*' * 84: # delimiter
+            if record: records.append(record)
+            record = {}
+        elif words[0] == '=' * 20 and words[2] == '=' * 20 + '<': # open tag
+            if field_name: raise RuntimeError(f'unexpected open tag at line {line_num}')
+            field_name = words[1]
+        elif words[0] == '=' * 20 and words[2] == '=' * 20 + '>': # close tag
+            if words[1] != field_name: raise RuntimeError(f'mismatched tag at line {line_num}')
+            record[field_name] = field_content.rstrip('\r\n')
+            field_content, field_name = '', None
+        elif field_name:
+            field_content += line
+        elif line.strip():
+            raise RuntimeError(f'unexpected content at line {line_num}: {line!r}')
+
+import xml.etree
+import xml.etree.ElementTree as ET
+root = ET.Element('functions')
+for record in records:
+    r = ET.SubElement(root, 'function')
+    for k, v in record.items():
+        n = ''.join(c.lower() + ('-' if c.islower() and d.isupper() else '') for c, d in zip(k, k[1:] + ' '))
+        try:
+            vt = ET.fromstring(f'<{n}>{v}</{n}>')
+            r.append(vt)
+        except xml.etree.ElementTree.ParseError:
+            f = ET.SubElement(r, n)
+            f.text = v
+ET.indent(root, ' ' * 4)
+print('<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n')
+ET.dump(root)
